480939008e3b3327b8779d544200f6dfb1498768
Shreyash dhage <sdhage1502@gmail.com>
2025-06-23 19:25:41 +0530

    fix env cause deploymen issue

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  "production": true,
+  "firebase": {
+    "apiKey": "AIzaSyDFsvw0ZpmTN3w0JPHYG3uUGiwY0r9m7zM",
+    "authDomain": "smartnoteapp-768e4.firebaseapp.com",
+    "projectId": "smartnoteapp-768e4",
+    "storageBucket": "jsmartnoteapp-768e4.firebasestorage.app",
+    "messagingSenderId": "391750140768",
+    "appId": "1:391750140768:web:b8aed4410bb41f8a0b9405"
+  }
+};
